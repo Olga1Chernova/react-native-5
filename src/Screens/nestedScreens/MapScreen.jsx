0542275c5c6b68015d060d8fb0e482d1da682ace
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-04-07 15:36:00 +0300

    updated MapScreen

--- src/Screens/nestedScreens/MapScreen.jsx
+++ src/Screens/nestedScreens/MapScreen.jsx
@@ -1,36 +1,16 @@
 import { Text, StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import * as Location from "expo-location";
 
 const MapScreen = () => {
-  const [location, setLocation] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        console.log("Permission to access location was denied");
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      const coords = {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      };
-      setLocation(coords);
-    })();
-  }, []);
-
-
   return (
     <View style={styles.container}>
       <MapView
         style={styles.mapStyle}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.006,
         }}
         mapType="standard"
         minZoomLevel={15}
@@ -38,9 +18,11 @@ const MapScreen = () => {
         onRegionChange={() => console.log("Region change")}
       >
         <Marker
-          title="I am here"
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          description="Hello"
+          coordinate={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
+          title={item.title}
         />
       </MapView>
     </View>
